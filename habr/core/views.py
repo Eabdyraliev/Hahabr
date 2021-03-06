a659bdd378dd468486a0f56416527e950ddc1433
Eabdyraliev <Eabdyraliev@gmail.com>
2021-03-06 11:30:51 +0600

    'Create_Hahabr'

---
+++ habr/core/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse
+from .models import Article
+
+# Create your views here.
+def homepage(request):
+    # return HttpResponse('<h2>Hello world!<h2>')
+    return render(request, 'index.html')
+
+def first_article(request):
+    article = Article.objects.first()
+    return render(
+        request,
+        'article_page.html',
+        {'article':article}
+    )
