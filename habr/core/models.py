4ea3fb8ed14a590b2ea3697df1371ed7831b560e
Eabdyraliev <Eabdyraliev@gmail.com>
2021-04-14 21:45:28 +0600

    commit march

--- habr/core/models.py
+++ habr/core/models.py
@@ -1,6 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
+    author = models.Foreign.Key(
+        to = 'Author'
+        on delete = models.SET_NULL
+        null=True, blank=True
+        verbose_name='Автор',
+        related_name='Автор'
+    )
+
+    readers = models.ManyToMany.Field(
+        to = User,
+        related_name = 'readed_articles',
+        blank = True,
+        verbose = name
+    )
+
+
+class Author(models.Models):
+    user = MOdels.OneToOneField(
+        to=User
+        relatedname='author',
+        verbos_name='пользователь',
+        null = False,
+        blank = False,
+        on_delete=models.Cascade)
+
+    nick = models.charfield(max_laength=55)
+
+    class Meta:
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+
+    def __str__(self):
+        return self.nick
--- habr/core/views.py
+++ habr/core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, HttpResponse
-from .models import Article
+from django.shortcuts import render, HttpResponse, redirect
+from django.db.models import Q
+from .models import Article, Author
+
 
 # Create your views here.
 def homepage(request):
@@ -13,3 +15,38 @@ def first_article(request):
         'article_page.html',
         {'article':article}
     )
+
+def edit_article(request, pk):
+    article = Article.objects.get(id=pk)
+
+    if request.method == "POST":
+        article.title = request.POST.get("title")
+        article.text = request.POST.get("text")
+        article.save()
+        return redirect(article_page, pk)
+    return render (request, "update.html", {"article": article})
+
+def add_article(request):
+    if request.method == "GET":
+        return render(request, "add.html")
+    elif request.method == "POST":
+        form = request.POST
+        title = form.get("title")
+        text = form.get("text")
+        
+        # new_article = Article(title=title, text=text)
+        # new_article.save()
+
+        new.article = Article()
+        new_article.title = title
+        new_article.text = text
+        user = request.user
+        author = user.author
+
+        new_article.save()
+        return redirect(article_page, new_article.pk)
+        
+
+def delete_article(request, id):
+    article = Article.object.get(pk=id)
+    article.delete()
